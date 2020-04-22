a30efc5336ff2d72b1e6589601f16471908f1f02
Glitch (hello-webpage) <none>
2020-04-22 22:47:30 +0000

    🌄🎖 Checkpoint ./dictionary.js:188030/3964494 ./index.html:188030/155 ./script.js:188030/909

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+/* global
+validWordTrie
+*/
+
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+console.log(isAValidWord('hello'));
+
+console.log(isAValidWord('nevan'));
+
+
+function isAValidWord(guess) {
+    let level = validWordTrie;
+    for (const letter of guess) {
+        level = level[letter];
+        if (!level) return false;
+    }
+    return '' in level;
+}
