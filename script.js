2f7ad78d5466c05f8884149574e49fabb1a9337a
Glitch (hello-webpage) <none>
2020-04-23 10:16:46 +0000

    👨🐈 Checkpoint ./script.js:188030/299

--- script.js
+++ script.js
@@ -12,8 +12,17 @@ console.log(isAValidWord("nevan"));
 console.log(wordsUsingLetters("radiwyz"));
 
 function wordsUsingLetters(letters) {
-  
+  for (const letter of letters) {
+    console.log(validWordTrie[letter]);
+  }
+  return true;
 }
+
+/*
+  1. Check for trees with the letters
+  2. If any of those trees have "", we've made a word!
+  3. Do this recursively
+*/
 
 function isAValidWord(guess) {
   let level = validWordTrie;
