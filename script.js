6c50270b4d7aa03231611ade5ed70c22c8069a77
Glitch (hello-webpage) <none>
2020-04-23 09:53:36 +0000

    💷🏂 Checkpoint ./script.js:188030/1067

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ validWordTrie
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+console.log(isAValidWord("hello"));
 
-console.log(isAValidWord('hello'));
+console.log(isAValidWord("nevan"));
 
-console.log(isAValidWord('nevan'));
+console.log(wordsUsingLetters("radiwyz"));
 
+function wordsUsingLetters(letters) {
+  
+}
 
 function isAValidWord(guess) {
-    let level = validWordTrie;
-    for (const letter of guess) {
-        level = level[letter];
-        if (!level) return false;
-    }
-    return '' in level;
+  let level = validWordTrie;
+  for (const letter of guess) {
+    level = level[letter];
+    if (!level) return false;
+  }
+  return "" in level;
 }
